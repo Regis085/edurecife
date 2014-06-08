ff1923b30468d39870c4020f5573e6498b9b0e91
José Regis <joseregisneto@gmail.com>
2014-06-08 14:41:12 -0300

    resolvendo menu

--- js/menu-flutuante.js
+++ js/menu-flutuante.js
@@ -1,12 +1,15 @@
 ﻿jQuery("document").ready(function ($) {
 
     var nav = $('.nav-original');
+    var container = $('#menu-container');
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 89) {
             nav.addClass("nav-flutua");
+            container.addClass("container-flutua");
         } else {
             nav.removeClass("nav-flutua");
+            container.removeClass("container-flutua");
         }
     });
 
