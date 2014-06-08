51cbfd5366731bd3909502f785452a95d4b3f562
José Regis <joseregisneto@gmail.com>
2014-06-08 14:57:39 -0300

    menu flutuante resolvido

--- js/menu-flutuante.js
+++ js/menu-flutuante.js
@@ -1,14 +1,14 @@
 ﻿jQuery("document").ready(function ($) {
 
-    var nav = $('.nav-original');
+    //var nav = $('.nav-original');
     var container = $('#menu-container');
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 89) {
-            nav.addClass("nav-flutua");
+            //nav.addClass("nav-flutua");
             container.addClass("container-flutua");
         } else {
-            nav.removeClass("nav-flutua");
+            //nav.removeClass("nav-flutua");
             container.removeClass("container-flutua");
         }
     });
